98980c4cc7261efb5c3415094553c2c708be91c6
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2020-10-02 23:59:06 +0200

    Add files via upload

---
+++ Config_for_supreme.py
@@ -0,0 +1,11 @@
+keys = {
+        "product_url": "https://www.supremenewyork.com/shop/accessories/w4lirj5v2",      
+        "name": "John Fisher",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "Forest Hills Dr.",
+        "city": "Gothenburg",
+        "zip_code": "91601",
+        "card_cvv": "666",
+        "card_number": "1228199669918221"
+}
